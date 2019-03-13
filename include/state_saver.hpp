3eeffe10b375c8f001c46ade8dac6ebc26170df3
terik23 <neargye@gmail.com>
2019-03-14 00:32:39 +0500

    v0.3.2

--- include/state_saver.hpp
+++ include/state_saver.hpp
@@ -5,7 +5,7 @@
 //  ____) | || (_| | ||  __/  ____) | (_| |\ V /  __/ |    | |____|_|   |_|
 // |_____/ \__\__,_|\__\___| |_____/ \__,_| \_/ \___|_|     \_____|
 // https://github.com/Neargye/state_saver
-// vesion 0.3.1
+// vesion 0.3.2
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 // SPDX-License-Identifier: MIT
